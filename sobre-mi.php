ee6eaa674136a772f4eb9b904665d28cea72e2ed
David Lidueña <davidliduena13@gmail.com>
2024-05-17 19:36:21 -0500

    cambios

--- sobre-mi.php
+++ sobre-mi.php
@@ -1,3 +1,10 @@
+<?php 
+  $pg = "sobre-mi";
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -12,35 +19,9 @@
 </head>
 <body id="sobre-mi">
     <header class="container">
-        <nav class="navbar navbar-expand-md mb-4">
-            <div class="container-fluid">
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                  <li class="nav-item">
-                    <a class="nav-link" href="index.html">Inicio</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link active" href="sobre-mi.html">Sobre mi</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="proyectos.html">Proyectos</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="contacto.html">Contacto</a>
-                  </li>
-                </ul>
-                <ul class="navbar-nav">
-                  <li class="float-sm-end">
-                      <a class="btn btn-rojo" href="contacto.html">Descargar mi CV <i
-                              class="fa-solid fa-download"></i></a>
-                  </li>
-              </ul>
-              </div>
-            </div>
-          </nav>
+    <?php
+        include_once("menu.php");
+      ?>
     </header>
 
     <main>
@@ -55,7 +36,7 @@
                   <img src="imagenes/foto.png" alt="foto-David" class="img-fluid">
               </div>
               <div class="col-6 d-sm-block d-sm-none py-3">
-                  <a href="contacto.html" class="btn btn-rojo my-5 d-sm-block d-sm-none">Enviar mensaje</a>
+                  <a href="contacto.php" class="btn btn-rojo my-5 d-sm-block d-sm-none">Enviar mensaje</a>
               </div>
           </div>
       </section>
